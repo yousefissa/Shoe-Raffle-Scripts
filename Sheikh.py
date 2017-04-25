78574e54d701639968528ac9d3a655595cd8c517
yousef issa <yousef@u.northwestern.edu>
2017-04-24 19:42:51 -0500

    Fixed crashing

--- Sheikh.py
+++ Sheikh.py
@@ -12,6 +12,7 @@ headers = {'User-Agent':
 session = requests.Session()
 session.headers.update(headers)
 
+
 # CHANGE the fields as the comments say  
 def send_form(limit):
     fid, sid, sitekey = sitekey_search()
@@ -19,16 +20,17 @@ def send_form(limit):
     api_key = '' # COPY YOUR 2CAPTCHA KEY HERE.
 
     for i in range(1, limit):
+        email = 'youremail+{}@gmail.com'.format(getrandbits(40)) # put your email in for your email
         cap_id = session.post("http://2captcha.com/in.php?key={}&method=userrecaptcha&googlekey={}&pageurl={}".format(api_key, sitekey, main_url)).text.split('|')[1]
         cap_answer = session.get("http://2captcha.com/res.php?key={}&action=get&id={}".format(api_key, cap_id)).text
         while 'CAPCHA_NOT_READY' in cap_answer:
             print('Waiting for captcha. Sleeping!')
-            sleep(5)
+            sleep(6)
             cap_answer = session.get("http://2captcha.com/res.php?key={}&action=get&id={}".format(api_key, cap_id)).text
-        return_cap = cap_answer.split('|')[1]
-
-        email = 'YOUR_EMAIL+{}@gmail.com'.format(getrandbits(40)) # CHANGE YOUR_EMAIL to your email prefix. don't change the +{} after.
-        
+        try: 
+            return_cap = cap_answer.split('|')[1]
+        except IndexError:
+            pass
         payload = {
             "fid": fid, # dont change
             "sid": sid, # dont change
@@ -36,17 +38,17 @@ def send_form(limit):
             "subid": "", # dont change
             "td": "", # dont change
             "formtype": "addcontact", # dont change
-            "90298[29288859]": "",  # PUT YOUR FIRST NAME HERE
-            "90299[29288860]": "", # PUT YOUR LAST NAME HERE
+            "90298[29288859]": '',  # PUT YOUR FIRST NAME HERE
+            "90299[29288860]": '', # PUT YOUR LAST NAME HERE
             "90300": email, # dont change 
-            "90301[29288866]" : "", # PUT YOUR SHOE SIZE HERE
+            "90301[29288866]" : "9.5", # PUT YOUR SHOE SIZE HERE
             "90302[29289136]": "", # dont change
             "90303[29289137]": "Agree to Terms", # dont change
             "90307[899049]": "true", # dont change
             "g-recaptcha-response": return_cap # dont change
         }
         resp = requests.post(main_url, data=payload, headers=headers)
-        print('{}/{} registered.'.format(i, limit))
+        print('{}/{} registered with {}.'.format(i, limit, email))
 
 # finds sitekey, sid, and fid
 # fid and sid are found in main_url... just in case it changes, i guess?
